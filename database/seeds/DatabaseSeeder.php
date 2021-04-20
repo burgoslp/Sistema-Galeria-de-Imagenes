582b55034525c10df137a3c45963ab1c8350635b
leopoldo <pinedo.burgoslp3@gmail.com>
2021-04-19 21:57:54 -0400

    se han creador los seeder para poblar las tablas estaticas

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+
+        $this->call(estatuTableSeeder::class);
+
+        $this->call(daCivileTableSeeder::class);
+        
+        $this->call(datSexoTableSeeder::class);
+
+        $this->call(roleTableSeeder::class);
+
+        $this->call(categoriaTableSeeder::class);
+
     }
 }
